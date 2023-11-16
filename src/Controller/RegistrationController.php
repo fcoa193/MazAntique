c1251624e083d257875e8a58908e76504c13f8d1
francois <francoismazerbourg@gmail.com>
2023-11-16 10:11:23 +0100

    final V1 before presentation

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -25,15 +25,20 @@ class RegistrationController extends AbstractController
     #[Route('/', name: 'app_register')]
     public function register(Request $request, EntityManagerInterface $entityManager, Security $security): Response
     {
+        // create a new User and a new Form
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
         $isUserConnected = false;
         $roleUser = '';
+
+        // Check if the user is connected
         if ($security->getUser() != null) {
             $isUserConnected = true;
             $roleUser = $security->getUser()->getRoles();
         }
+
+        // If the form is valid and submitted Hash the password with the service
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setPassword(
                 $this->passwordService->hashPassword(
@@ -41,9 +46,13 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+
+        // Give the role User 
             $user->setRoles(['ROLE_USER']);
             $entityManager->persist($user);
             $entityManager->flush();
+            
+        // Add a flash message
             $this->addFlash('success', 'Inscription réussi! Vous pouvez maintenant vous connecter.');
             return $this->redirectToRoute('app_home');
         }
